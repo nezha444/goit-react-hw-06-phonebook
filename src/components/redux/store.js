6f1ec8661684ea391264657c1c37a5a30aa93de2
Nikita <bazilik2452@gmail.com>
2023-06-16 08:40:00 +0200

    /

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import { createStore } from 'redux';
 import { reducer } from './reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-import { persistStore, persistReducer } from 'redux-persist';
+// import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 // Редьюсер который принимает state и action, перебирает и провереряет по типу(type)
@@ -41,6 +51,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export const persistor = persistStore(store);
